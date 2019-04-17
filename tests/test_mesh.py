d7e971033a9a468645cae89f1f1141665c564196
Adrian Croucher <a.croucher@auckland.ac.nz>
2019-04-18 11:53:32 +1200

    Add mesh surface tests (and remove some meshio tests)

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -12,6 +12,7 @@ class meshTestCase(unittest.TestCase):
         self.assertEqual(m.num_nodes, 16)
         self.assertEqual(m.num_columns, 9)
         self.assertEqual(m.num_layers, 3)
+        self.assertEqual(m.num_cells, 27)
 
         self.assertEqual(np.linalg.norm(
             m.node[0].pos), 0.)
@@ -29,6 +30,25 @@ class meshTestCase(unittest.TestCase):
         for lay, centre in zip(m.layer, layer_centres):
             self.assertEqual(lay.centre, centre)
 
+    def test_surface(self):
+
+        dx = [10.]*3; dy = [12.] * 3
+        dz = [1., 2., 3.]
+        m = mesh(columns = [dx, dy], layers = dz)
+
+        surface = {4: -1}
+        m = mesh(columns = [dx, dy], layers = dz, surface = surface)
+        self.assertEqual(m.num_cells, 26)
+
+        surface = [-3] * 9
+        m = mesh(columns = [dx, dy], layers = dz, surface = surface)
+        self.assertEqual(m.num_cells, 9)
+
+        surface = [0.2, -0.8, -1.5] * 3
+        m = mesh(columns = [dx, dy], layers = dz, surface = surface)
+        nlay = [col.num_layers for col in m.column]
+        self.assertEqual(nlay, [3, 2, 2] * 3)
+
     def test_meshio_points_cells(self):
 
         dx = [10.]*3; dy = [12.] * 3
@@ -38,15 +58,11 @@ class meshTestCase(unittest.TestCase):
         points, cells = m.meshio_points_cells
         self.assertEqual(len(points), 16 * 4)
         self.assertEqual(len(cells['hexahedron']), 9 * 3)
-        self.assertEqual(np.linalg.norm(points[0]), 0.)
-        self.assertEqual(
-            np.linalg.norm(points[-1] -
-                           np.array([30., 36., -6])), 0.)
-        self.assertEqual(list(cells['hexahedron'][0]),
-                         [16, 20, 21, 17, 0, 4, 5, 1])
-        self.assertEqual(list(cells['hexahedron'][-1]),
-                         [58, 62, 63, 59, 42, 46, 47, 43])
 
+        surface = [0.2, -0.8, -1.5] * 3
+        m = mesh(columns = [dx, dy], layers = dz, surface = surface)
+        points, cells = m.meshio_points_cells
+        self.assertEqual(len(cells['hexahedron']), 21)
 
 if __name__ == '__main__':
 
