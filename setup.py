1af0d68978f488494ebfed6926cd3d4fe763a81c
Adrian Croucher <a.croucher@auckland.ac.nz>
2020-02-20 13:46:36 +1300

    Add classifiers etc. to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
-setup(name='layermesh',
-      version='0.1',
-      description='Library for layered computational meshes',
-      url='http://github.com/acroucher/layermesh',
-      author='Adrian Croucher',
-      author_email='a.croucher@auckland.ac.nz',
-      license='LGPL',
-      packages=['layermesh'],
-      install_requires=['numpy'],
-      zip_safe=False)
+setup(
+    name='layermesh',
+    version='0.1',
+    description='Library for layered computational meshes',
+    url='http://github.com/acroucher/layermesh',
+    author='Adrian Croucher',
+    author_email='a.croucher@auckland.ac.nz',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent"],
+    install_requires=['numpy']
+)
