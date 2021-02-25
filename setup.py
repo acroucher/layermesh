f5092219567993b98aeccd20b60afbe57de43601
Adrian Croucher <a.croucher@auckland.ac.nz>
2021-02-25 15:31:31 +1300

    Add matplotlib to requirements

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent"],
-    install_requires=['numpy', 'scipy', 'h5py', 'meshio']
+    install_requires=['numpy', 'scipy', 'h5py', 'meshio', 'matplotlib']
 )
