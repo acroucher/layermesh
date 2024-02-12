61d91f65cc9187fcc323fd9ba655b191c283eb39
Adrian Croucher <a.croucher@auckland.ac.nz>
2024-02-12 14:49:50 +1300

    Change packaging to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-import setuptools
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setuptools.setup(
-    name='layermesh',
-    version='0.3.3',
-    description='Library for layered computational meshes',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='http://github.com/acroucher/layermesh',
-    author='Adrian Croucher',
-    author_email='a.croucher@auckland.ac.nz',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Operating System :: OS Independent"],
-    python_requires='>=2.7',
-    install_requires=['numpy', 'scipy', 'h5py', 'meshio', 'matplotlib']
-)
+from setuptools import setup
+# for backwards compatibility only
+setup()
